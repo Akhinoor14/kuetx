550e7572fc85f6005284467d1cdd1bd0bec48b47
Akhinoor14 <mdakhinoorislam.official.2005@gmail.com>
2026-06-03 02:21:38 +0600

    dele

--- tmp_print_lines.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const p='d:/Skill/kuetx/src/pages/About.jsx';
-const lines = fs.readFileSync(p,'utf8').split('\n');
-for(let i=0;i<lines.length;i++) console.log((i+1).toString().padStart(3)+': '+lines[i]);
