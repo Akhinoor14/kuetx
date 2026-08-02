54b0ad1610e63149d19b298547ef74ea7b7a039f
Md. Akhinoor Islam <islam2313014@stud.kuet.ac.bd>
2026-08-02 13:13:17 +0600

    feat: add ESE_NOTES with approvals from academic council meetings

---
+++ src/data/curriculum/departments/ESE/notes.js
@@ -0,0 +1,12 @@
+export const ESE_NOTES = {
+  approvals: [
+    'Detailed outline of first year first term was approved in the 54th meeting of the academic council.',
+    'Detailed outline of first year second term was approved in the 55th meeting of the academic council.',
+    'Detailed outline of 2nd year first and second term was approved in the 57th meeting of the academic council.',
+    'Course structure was approved in the 57th meeting of the academic council.',
+    'Detailed outline of 3rd year first and second term was approved in the 61st meeting of the academic council.',
+    'Course titles of ESE 4105, ESE 4013 were modified in the 66th meeting of the academic council.',
+    'Three optional courses (ESE 4017, ESE 4075 and ESE 4077) were approved in the 61st meeting of the academic council.',
+    'Detailed outline of 4th year first and second term was approved in the 61st meeting of the academic council.',
+  ],
+};
